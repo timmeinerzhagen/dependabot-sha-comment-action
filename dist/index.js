0c69a35e633d2127c1d9ccfc0863f7e76f35eb3b
Tim Jonas Meinerzhagen <timmeinerzhagen@gmail.com>
2021-11-04 04:25:01 +0100

    Build

--- dist/index.js
+++ dist/index.js
@@ -87,6 +87,7 @@ function run() {
                             const sp = output.split(line);
                             const newline = line.substring(1).split('#')[0] + ' # ' + version_update + '\n';
                             const newfile = sp[0] + newline + sp[1];
+                            core.info("Start File");
                             core.info(newfile);
                             yield exec.exec('echo ', ['\"' + newfile + '\"', '>', path], options);
                             yield exec.exec('git ', ['config', '--global', 'user.name', 'GitHub Actions'], options);
