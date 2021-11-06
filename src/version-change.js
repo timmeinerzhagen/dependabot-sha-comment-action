4f10ad6f526c96ad4ef1b0f1bc0d46bbfe737a04
Tim Jonas Meinerzhagen <timmeinerzhagen@gmail.com>
2021-11-06 17:37:31 +0100

    fix: move gh actions check to check version

--- src/version-change.js
+++ src/version-change.js
@@ -4,7 +4,13 @@ module.exports = async ({process, context, exec}) => {
 
     const titleparts = payload.pull_request.title.split('to')
     const version_update = titleparts[titleparts.length - 1].trim();
+
+    // Check if GitHub Actions file is changes //
     const path = DIFF.split('\n')[0].split(' ')[2].substr(2);
+    if (!(path.startsWith('.github/workflows') && (path.endsWith('.yml') || path.endsWith('.yaml')))) {
+        core.info("Not a GitHub Actions Dependabot Pull Request.");
+        return;
+    }
 
     // Look through DIFF for version change //
     for(const line of DIFF.split('\\n')) {
@@ -13,22 +19,23 @@ module.exports = async ({process, context, exec}) => {
 
             // If version is SHA, then add comment //
             if(version.length == 40) {
-            let output = '', error = '';
-            const options = {
-                listeners: { 
-                stdout: (data) => { output += data.toString(); },
-                stderr: (data) => { error += data.toString(); }
-                },
-                cwd: './'
-            };
+                let output = '', error = '';
+                const options = {
+                    listeners: { 
+                        stdout: (data) => { output += data.toString(); },
+                        stderr: (data) => { error += data.toString(); }
+                    },
+                    cwd: './'
+                };
 
-            // Calculate changes //
-            await exec.exec('cat ', [path], options);
-            const newline = line.substring(1).split('#')[0].trim() + ' # ' + version_update + '\n';
+                // Calculate changes //
+                await exec.exec('cat ', [path], options);
+                const newline = line.substring(1).split('#')[0].trim() + ' # ' + version_update + '\n';
 
-            // Save changes in file //
-            await exec.exec('sed -i \"s/' + line.substring(1).trim().replace("/", "\\/") + '/' + newline.trim().replace("/", "\\/") + '/g\" ' + path);
+                // Save changes in file //
+                await exec.exec('sed -i \"s/' + line.substring(1).trim().replace("/", "\\/") + '/' + newline.trim().replace("/", "\\/") + '/g\" ' + path);
             }
         }
     }
+    return true;
 }
